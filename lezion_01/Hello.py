6d21f062df494736a06b836dbabeb8f34f102095
Haukeys <leopazienza76@gmail.com>
2025-02-14 13:14:57 +0100

    second commit

--- lezion_01/Hello.py
+++ lezion_01/Hello.py
@@ -1,1 +1,25 @@
 print("Hello Word")
+#first code
+
+# I modo
+print("hello world")
+print("hello world")
+print("hello world")
+
+#II modo ciclo while
+print("while:")
+
+i:int=1 
+
+while i<=5:
+    print("hello world")
+    i=i+1 
+    
+#ciclo for
+print("for:")
+for i in range(5):
+    print("hello world")
+    
+#example
+for i in range(1,11,2):
+    print(i)
