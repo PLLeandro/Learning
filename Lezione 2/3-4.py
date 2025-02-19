8242ea080813a7d9e43c20c7546d8b0f8da2558f
Haukeys <leopazienza76@gmail.com>
2025-02-19 19:24:11 +0100

    15th commit

--- Lezione 2/3-4.py
+++ Lezione 2/3-4.py
@@ -2,5 +2,6 @@
 
 people_invited=["Bil Gates", "Hideo kojyma", "Scarlette Johanson"]
 
-for people_invite in people_invited:
-    print(f"Dear {people_invite}, I would like to invite you for a dinner with me.")
+print(f"Dear {people_invited[0]}I would like to invite you for a dinner with me.")
+print(f"konichiwa {people_invited[1]} , please be my guest for a dinner.")
+print(f"Greeting {people_invited[2]} , i would like a date with you and a bunch of friends.")
